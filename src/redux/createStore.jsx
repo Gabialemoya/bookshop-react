b5537ef095a6ce9f7aae0845d70f3e17c8e1dcff
gabialemoya <gmgabiale72@gmail.com>
2021-07-17 20:10:58 -0300

    persistencia de carrito

--- src/redux/createStore.jsx
+++ src/redux/createStore.jsx
@@ -4,6 +4,7 @@ import thunk from 'redux-thunk';
 import createSagaMiddle from 'redux-saga';
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
+import {persistStore} from 'redux-persist';
 
 const sagaMiddleware = createSagaMiddle();
 
@@ -11,4 +12,9 @@ export const middlewares = [thunk, sagaMiddleware, logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export const persistor = persistStore(store);
+
+export default {
+    store,
+    persistor
+};
