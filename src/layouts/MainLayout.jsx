cb979a245cd7c657cb939b598d5aa65ea29bbbd2
Rocio Pepek <rocipepek@gmail.com>
2021-07-28 22:39:48 -0300

    Ajustes Footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import Header from './../components/Header/header';
-import Footer from './../components/Footer/footer';
+import React from "react";
+import Header from "./../components/Header/header";
+import Footer from "./../components/Footer/footer";
 
-const MainLayout = props => {
-
-    return(
-        <div >
-            <Header {...props}/>
-            <div className="main">
-                {props.children}
-            </div>
-          <Footer/> 
-        </div>
-    );
+const MainLayout = (props) => {
+  return (
+    <div className="container">
+      <Header {...props} />
+      <div className="main">{props.children}</div>
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
