ab98780d9ae784e35ef6bf3b38aa0ef0dd74d3af
Diego <diego-jofre@live.com.ar>
2021-08-17 23:47:33 -0300

    envio de mail listo

--- src/components/Confirm/confirm.jsx
+++ src/components/Confirm/confirm.jsx
@@ -1,39 +1,25 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import UserProfile from './../UserProfile/userprofile';
-import {  useHistory } from 'react-router-dom';
-import Button from '../forms/Button/button';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Button from "../forms/Button/button";
 
-import './confirm.scss';
+import "./confirm.scss";
 
-const mapState = ({user}) => ({
-  currentUser: user.currentUser
-})
-const Confirm = ({children}) => {
-  const { currentUser } = useSelector(mapState);
+const Confirm = () => {
   const history = useHistory();
 
   const handleHome = () => {
-    
-    history.push('/');
-}
-
-
-  const name = {
-    currentUser
-  }
+    history.push("/");
+  };
 
   return (
     <div>
-      
-        <h1>
-            Gracias  por comprar!! Te mandamos un mail con la informacion de la compra :) 
-        </h1>
-      <Button onClick={() => handleHome()}>
-        Ir a Inicio
-      </Button>
+      <h1>
+        Gracias por comprar!! Te mandamos un mail con la informacion de la
+        compra :)
+      </h1>
+      <Button onClick={() => handleHome()}>Ir a Inicio</Button>
     </div>
   );
-}
+};
 
 export default Confirm;
--- src/components/Signup/signup.jsx
+++ src/components/Signup/signup.jsx
@@ -1,143 +1,137 @@
-import React, {useState, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {withRouter, useHistory} from 'react-router-dom';
-import {signUpUser, signUpUserStart} from './../../redux/User/user.actions';
-import './signup.scss';
+import React, { useState, useEffect } from "react";
+import emailjs from "emailjs-com";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { signUpUserStart } from "./../../redux/User/user.actions";
+import "./signup.scss";
 
-// import {auth, handleUserProfile} from './../../firebase/utils';
-import AuthWrapper from './../AuthWrapper/authwrapper';
-import FormInput from './../forms/FormInput/forminput';
-import Button from './../forms/Button/button';
+import AuthWrapper from "./../AuthWrapper/authwrapper";
+import FormInput from "./../forms/FormInput/forminput";
+import Button from "./../forms/Button/button";
 
-//object
-// const initialState = {
-//     displayName: '',
-//     email: '',
-//     password: '',
-//     confirmPassword:'',
-//     errors: []
-// };
-
-const mapState = ({user}) => ({
-    currentUser: user.currentUser,
-    useErr: user.useErr
-    //signUpSuccess: user.signUpSuccess,
-   // signUpError: user.signUpError
+const mapState = ({ user }) => ({
+  currentUser: user.currentUser,
+  useErr: user.useErr,
 });
 
-const Signup = props => {
-    const history = useHistory();
-    //const {signUpSuccess, signUpError} = useSelector(mapState);
-    const {currentUser, useErr} = useSelector(mapState);
-    const dispatch = useDispatch();
-    const [displayName, setDisplayName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+const Signup = (props) => {
+  const history = useHistory();
+  const { currentUser, useErr } = useSelector(mapState);
+  const dispatch = useDispatch();
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errors, setErrors] = useState([]);
 
-    //hook
-    useEffect(() => {
-        if(currentUser){
-        //if(signUpSuccess){
-            reset();
-         //   dispatch(resetAllAuthForms());
-            history.push('/');
-        }
-   // }, [signUpSuccess]);
-    }, [currentUser]);
-
-    useEffect(() => {
-        if(Array.isArray(useErr) && useErr.length>0){
-            setErrors(useErr);
-        }
-    }, [useErr]);
-
-    //vaciar formulario
-    const reset = () =>{
-        setDisplayName('');
-        setEmail('');
-        setPassword('');
-        setConfirmPassword('');
-        setErrors([]);
-    };
-    
-
-   const handleFormSubmit = event => {
-        event.preventDefault();
-        dispatch(signUpUserStart({
-            displayName,
-            email,
-            password,
-            confirmPassword
-        }));
-        
-
-
+  //hook
+  useEffect(() => {
+    if (currentUser) {
+      reset();
+      history.push("/");
     }
+  }, [currentUser]);
 
-    
-        //const {displayName, email, password, confirmPassword, errors} = this.state;
-
-        const configAuthWrapper = {
-            headline: 'Crear cuenta'
-        };
-
-        return(
-            <AuthWrapper {...configAuthWrapper}>
-                <div className="formWrap">
-
-                    {errors.length >0 &&(
-                        <ul>
-                            {errors.map((err, index) => {
-                                return(
-                                    <li key={index}>
-                                        {err}
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    )}
-
-                        <form onSubmit={handleFormSubmit}>
-                            <FormInput
-                                type="text"
-                                name="displayName"
-                                value={displayName}
-                                placeholder="Ingrese su nombre completo"
-                                handleChange={e => setDisplayName(e.target.value)}
-                            />
-                            <FormInput
-                                type="email"
-                                name="email"
-                                value={email}
-                                placeholder="Ingrese su correo electrónico"
-                                handleChange={e => setEmail(e.target.value)}
-                            />
-                            <FormInput
-                                type="password"
-                                name="password"
-                                value={password}
-                                placeholder="Ingrese una contraseña"
-                                handleChange={e => setPassword(e.target.value)}
-                            />
-                            <FormInput
-                                type="password"
-                                name="confirmPassword"
-                                value={confirmPassword}
-                                placeholder="Confirme la contraseña"
-                                handleChange={e => setConfirmPassword(e.target.value)}
-                            />
-
-                            {/* no usar una contraseña demasiado corta porque se rompe */}
-                            <Button type="submit">
-                                Register
-                            </Button>
-                        </form>
-                    </div>
-            </AuthWrapper>
-        )
+  useEffect(() => {
+    if (Array.isArray(useErr) && useErr.length > 0) {
+      setErrors(useErr);
     }
+  }, [useErr]);
+
+  //vaciar formulario
+  const reset = () => {
+    setDisplayName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+    setErrors([]);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    dispatch(
+      signUpUserStart({
+        displayName,
+        email,
+        password,
+        confirmPassword,
+      })
+    );
+    sendEmail(event);
+    event.target.reset();
+  };
+
+  const sendEmail = (e) => {
+
+    emailjs
+      .sendForm(
+        "gmail",
+        "template_lb3ie7k",
+        e.target,
+        "user_PqBRce5EDpXMks8EICz3g"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  const configAuthWrapper = {
+    headline: "Crear cuenta",
+  };
+
+  return (
+    <AuthWrapper {...configAuthWrapper}>
+      <div className="formWrap">
+        {errors.length > 0 && (
+          <ul>
+            {errors.map((err, index) => {
+              return <li key={index}>{err}</li>;
+            })}
+          </ul>
+        )}
+
+        <form onSubmit={handleFormSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            placeholder="Ingrese su nombre completo"
+            handleChange={(e) => setDisplayName(e.target.value)}
+          />
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Ingrese su correo electrónico"
+            handleChange={(e) => setEmail(e.target.value)}
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Ingrese una contraseña"
+            handleChange={(e) => setPassword(e.target.value)}
+          />
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            placeholder="Confirme la contraseña"
+            handleChange={(e) => setConfirmPassword(e.target.value)}
+          />
+
+          {/* no usar una contraseña demasiado corta porque se rompe */}
+          <Button type="submit">Register</Button>
+        </form>
+      </div>
+    </AuthWrapper>
+  );
+};
 //}
 
 export default Signup;
