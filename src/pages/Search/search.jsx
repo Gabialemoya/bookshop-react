67b8bee54d509743901a643f48dacec554326b0d
Rocio Pepek <rocipepek@gmail.com>
2021-12-02 23:05:30 -0300

    Ajustes css

--- src/pages/Search/search.jsx
+++ src/pages/Search/search.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ProductResults from './../../components/ProductResults/productresults';
+import React from "react";
+import ProductResults from "./../../components/ProductResults/productresults";
+import "./search.scss";
 
-const Search = ({}) => {
-    return (
-        <div className="searchPage">
-            <ProductResults/>
-        </div>
-    );
+const Search = () => {
+  return (
+    <div className="searchPage">
+      <ProductResults />
+    </div>
+  );
 };
 
 export default Search;
