f8b9f22338bf1ba260868426a7138ec2b815bf4d
Rocio Pepek <rocipepek@gmail.com>
2021-12-13 02:13:00 -0300

    Modificacion url para restablecer contraseña

--- src/redux/User/user.helpers.jsx
+++ src/redux/User/user.helpers.jsx
@@ -1,34 +1,36 @@
-import {auth} from './../../firebase/utils';
+import { auth } from "./../../firebase/utils";
 
 export const handleResetPasswordAPI = (email) => {
-    //const {email}=this.state;
-    const config ={
-        //pagina a la que mandamos al usuario una vez que 
-        // modifico la contraseña
-        url: 'https://bookshop-react.netlify.app/login'
-    };
+  //const {email}=this.state;
+  const config = {
+    //pagina a la que mandamos al usuario una vez que
+    // modifico la contraseña
+    url: "http://localhost:3000/login",
+  };
 
-    return new Promise((resolve, reject) => {
-        // sendPasswordResetEmail recibe el mail del usuario 
-          // y el objeto de configuracion (config)
-          auth.sendPasswordResetEmail(email, config)
-          // then -> callback
-      
-          .then(() => { //en el caso que funcione que hace
-             resolve();
-              })
-              // props.history.push('/login'); //redirije al login
-          
-          .catch(() => { //en el caso que falle que hace
-              //console.log('No se pudo cambiar la contraseña');
-              const err = ['Cuenta inexistente. Intente de nuevo.'];
-            reject(err);
-             
-              // this.setState({
-              //     errors:err
-              // });
-              // setErrors(err);
-          });
+  return new Promise((resolve, reject) => {
+    // sendPasswordResetEmail recibe el mail del usuario
+    // y el objeto de configuracion (config)
+    auth
+      .sendPasswordResetEmail(email, config)
+      // then -> callback
 
-    });
+      .then(() => {
+        //en el caso que funcione que hace
+        resolve();
+      })
+      // props.history.push('/login'); //redirije al login
+
+      .catch(() => {
+        //en el caso que falle que hace
+        //console.log('No se pudo cambiar la contraseña');
+        const err = ["Cuenta inexistente. Intente de nuevo."];
+        reject(err);
+
+        // this.setState({
+        //     errors:err
+        // });
+        // setErrors(err);
+      });
+  });
 };
